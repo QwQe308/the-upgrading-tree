08be4edc25a4cbd518c4e77053a222e8ba13650b
QwQe308 <78683793+QwQe308@users.noreply.github.com>
2022-01-01 16:43:51 +0800

    Update u1_row2.js

--- js/layers/u1_row2.js
+++ js/layers/u1_row2.js
@@ -160,7 +160,7 @@ addLayer("g", {
         12: {
             description: "总升级点加成时间速率.",
             effect(){
-                var eff = player.u1.total
+                var eff = player.u1.total.div(10).pow(2).add(1)
                 return eff
             },
             effectDisplay(){return `x${format(this.effect())}`},
