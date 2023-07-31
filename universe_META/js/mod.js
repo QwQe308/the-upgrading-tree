559da58de260ee2b9a6d19a5d2157dd051c3ba75
QwQe308 <78683793+QwQe308@users.noreply.github.com>
2023-07-31 13:48:30 +0800

    v1.02

--- universe_META/js/mod.js
+++ universe_META/js/mod.js
@@ -109,4 +109,6 @@ function maxTickLength() {
 
 // Use this if you need to undo inflation from an older version. If the version is older than the version that fixed the issue,
 // you can cap their current resources with this.
-function fixOldSave(oldVersion){}
+function fixOldSave(oldVersion){
+    if(oldVersion == 1.01) player.u.unlocked = false
+}
